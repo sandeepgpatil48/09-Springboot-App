c2708f744cf1745c3ddcbd552674722747552f6c
sandeep <sandeeppatil776@gmail.com>
2022-12-29 11:15:36 +0530

    sagar saved successfully

--- src/main/java/com/sandeep/Application.java
+++ src/main/java/com/sandeep/Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		
+        public void doProcess(){
+        
+         System.out.println("Haello World");
+      } 
+         SpringApplication.run(Application.class, args);
 	}
 
       public void doProcess()
